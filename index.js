082946b823e8e44f99e65db472495c8a72e5743a
= <csbenjamin.math@gmail.com>
2015-06-12 17:48:44 -0300

    remove unused code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var deprecate = require('util').deprecate;
-
 // this is used only to test
 module.exports = function mixin(app) {
   app.loopback.modelBuilder.mixins.define('Tree', require('./tree'));
